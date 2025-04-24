390e99f5a04a834135d1dc1b7af2564a2715f280
jill <siswoyo.fira@gmail.com>
2025-04-24 12:27:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Latihan membuat web")
+st.title("Latihan membuat web Fauzi")
 st.write(
     "Latihan membuat website melalui Github dan Streamlit dengan menggunakan bahasa Python"
 )

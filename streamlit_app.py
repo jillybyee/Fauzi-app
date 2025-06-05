dce70000f5092a0049853bc5807a3da98923a847
jill <siswoyo.fira@gmail.com>
2025-06-05 19:04:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("Latihan membuat web Fauzi")
 st.write(
     "Latihan membuat website melalui Github dan Streamlit dengan menggunakan bahasa Python"
 )
-st.image("Screenshot_20250601_224645_WhatsApp.jpg", width = 150);
+st.image("Screenshot_20250601_224645_WhatsApp.jpg", width = 150)
